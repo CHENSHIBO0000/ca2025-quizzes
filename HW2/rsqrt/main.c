28cc4490fa82b6e7a138bb4f7c1317edbdf3a53d
CHENSHIBO0000 <bbchen0517@gmail.com>
2025-11-03 10:42:57 +0800

    Refactor multiplication functions for RV32I

--- HW2/rsqrt/main.c
+++ HW2/rsqrt/main.c
@@ -3,26 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define printstr(ptr, length)                   \
-    do {                                        \
-        asm volatile(                           \
-            "add a7, x0, 0x40;"                 \
-            "add a0, x0, 0x1;" /* stdout */     \
-            "add a1, x0, %0;"                   \
-            "mv a2, %1;" /* length character */ \
-            "ecall;"                            \
-            :                                   \
-            : "r"(ptr), "r"(length)             \
-            : "a0", "a1", "a2", "a7");          \
-    } while (0)
+static inline void write_str_syscall(int fd, const char *ptr, long len) {
+    register long a0 asm("a0") = fd;
+    register const char *a1 asm("a1") = ptr;
+    register long a2 asm("a2") = len;
+    register long a7 asm("a7") = 64;   // system_write
 
-#define TEST_OUTPUT(msg, length) printstr(msg, length)
+    asm volatile("ecall"
+                 : "+r"(a0)              
+                 : "r"(a1), "r"(a2), "r"(a7)
+                 : "memory", "cc");      
+}
 
-#define TEST_LOGGER(msg)                     \
-    {                                        \
-        char _msg[] = msg;                   \
-        TEST_OUTPUT(_msg, sizeof(_msg) - 1); \
-    }
+#define printstr(ptr, length) write_str_syscall(1, (ptr), (length))
+
+#define TEST_OUTPUT(msg, length) printstr((msg), (length))
+
+#define TEST_LOGGER(msg)                          \
+do {                                              \
+    static const char _s_[] = msg;                \
+    TEST_OUTPUT(_s_, (long)(sizeof(_s_) - 1));    \
+} while (0)
 
 extern uint64_t get_cycles(void);
 extern uint64_t get_instret(void);
@@ -170,13 +171,50 @@ static int clz(uint32_t x){
 
 static uint64_t mul32(uint32_t a, uint32_t b)
 {
-    uint64_t result = 0;
-    for(int i = 0; i < 32; i++) {
+    uint32_t lo = 0;
+    uint32_t hi = 0;
+
+    for (int i = 0; i < 32; i++) {
         if (b & (1U << i)) {
-            result += ((uint64_t)a << i);
+            uint32_t part_lo = a << i;
+            uint32_t part_hi = (i == 0) ? 0 : (a >> (32 - i));
+            lo += part_lo;
+
+            if (lo < part_lo)
+                hi++;
+
+            hi += part_hi;
         }
     }
-    return result;
+
+    return ((uint64_t)hi << 32) | lo;
+}
+
+
+static inline void mul32_lohi(uint32_t a, uint32_t b,
+                              uint32_t *hi, uint32_t *lo)
+{
+    uint32_t L = 0, H = 0;
+    for (int i = 0; i < 32; i++) {
+        if (b & (1u << i)) {
+            uint32_t part_lo = a << i;
+            uint32_t part_hi = (i == 0) ? 0 : (a >> (32 - i));
+
+            uint32_t oldL = L;
+            L += part_lo;
+            if (L < oldL) H++;
+
+            H += part_hi;
+        }
+    }
+    *hi = H; *lo = L;
+}
+
+
+static inline uint32_t shr64_lo32(uint32_t hi, uint32_t lo, uint32_t s)
+{
+    if (s == 0) return lo;
+    return (lo >> s) | (hi << (32 - s));
 }
 
 static const uint32_t rsqrt_table[32] = {
